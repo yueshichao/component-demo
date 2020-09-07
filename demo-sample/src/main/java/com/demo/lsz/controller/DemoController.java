6d95c11bae65e0b33614a4f743743032fef79969
leshichao <leshichao@kedacom.com>
2020-09-07 10:54:55 +0800

    slf4j冲突

--- demo-sample/src/main/java/com/demo/lsz/controller/DemoController.java
+++ demo-sample/src/main/java/com/demo/lsz/controller/DemoController.java
@@ -1,14 +1,22 @@
 package com.demo.lsz.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
+@Slf4j
 public class DemoController {
 
     @GetMapping("/demo")
     public String demo() {
-        return "demo" + System.currentTimeMillis();
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        log.info("demo ===> {}", sdf.format(date));
+        return "demo" + date.getTime();
     }
 
 }

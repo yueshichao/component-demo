592d7ff91a92f1161c9ce63be626876fbc8e7fb5
yueshichao <704978457@qq.com>
2021-04-28 18:19:09 +0800

    redis lua 脚本执行

--- lsz-lua-script/src/main/java/com/lsz/redis/lua/MyRedisLock.java
+++ lsz-lua-script/src/main/java/com/lsz/redis/lua/MyRedisLock.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RScript;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 
 import java.util.Collections;
 import java.util.UUID;
@@ -21,6 +22,8 @@ public class MyRedisLock implements Lock {
 
     private UUID uuid = UUID.randomUUID();
 
+    private int expireTime = 1000 * 1000;
+
 
     @Override
     public void lock() {
@@ -33,8 +36,7 @@ public class MyRedisLock implements Lock {
         * 不是重入则返回锁剩余时间
         *
         * */
-//        TODO 乱码
-        Object eval = client.getScript().eval(RScript.Mode.READ_WRITE,
+        Object eval = client.getScript(new StringCodec()).eval(RScript.Mode.READ_WRITE,
                 "if (redis.call('exists', KEYS[1]) == 0) then\n" +
                         "    redis.call('hincrby', KEYS[1], ARGV[2], 1);\n" +
                         "    redis.call('pexpire', KEYS[1], ARGV[1]);\n" +
@@ -48,7 +50,7 @@ public class MyRedisLock implements Lock {
                         "return redis.call('pttl', KEYS[1]);",
                 RScript.ReturnType.STATUS,
                 Collections.singletonList(name),
-                "100000", lockId
+                expireTime + "", lockId
         );
         if (eval != null) {
             log.debug("lock, type = {}, result ===> {}", eval.getClass().getSimpleName(), eval.toString());
@@ -74,7 +76,34 @@ public class MyRedisLock implements Lock {
 
     @Override
     public void unlock() {
-
+        String lockId = uuid.toString() + ":" + Thread.currentThread().getId();
+        log.info("lockId = {}", lockId);
+        /*
+         * 解锁流程：
+         * 先判断锁是不是自己的
+         * 不是则直接不能解
+         * 是则将计数减一，如果减到0则删除此key
+         * TODO
+         * */
+        Object eval = client.getScript(new StringCodec()).eval(RScript.Mode.READ_WRITE,
+                "if(redis.call('hexists', KEYS[1], ARGV[2]) == 0) then return nil; end\n" +
+                        "local counter = redis.call('hincry', KEYS[1], ARGV[2], 1);\n" +
+                        "if(counter > 0) then\n" +
+                        "    redis.call('pexpire', KEYS[1], ARGV[1]);\n" +
+                        "    return 0;\n" +
+                        "else\n" +
+                        "    redis.call('del', KEYS[1]);\n" +
+                        "    return 1;\n" +
+                        "end",
+                RScript.ReturnType.STATUS,
+                Collections.singletonList(name),
+                expireTime + "", lockId
+        );
+        if (eval != null) {
+            log.debug("lock, type = {}, result ===> {}", eval.getClass().getSimpleName(), eval.toString());
+        } else {
+            log.debug("lock, result ===> null");
+        }
     }
 
     @Override
